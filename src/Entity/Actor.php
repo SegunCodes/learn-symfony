12dbb8fcb43631d72a382a87000c60237f056f54
SegunCodes <shegstix64@gmail.com>
2022-11-14 09:42:49 +0100

    add entity and doctrine

---
+++ src/Entity/Actor.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ActorRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ActorRepository::class)]
+class Actor
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    // #[ORM\ManyToMany(targetEntity=Movies::class, mappedBy:"actors")]
+    // private $movies;
+
+    // public function __construct()
+    // {
+    //     $this->movies = new ArrayCollection();
+    // }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    // public function getMovies(): Collection
+    // {
+    //     return $this->movies;
+    // }
+
+    // public function addMovie(Movies $movie): self
+    // {
+    //     if(!$this->movies->contains($movie)){
+    //         $this->movies[] = $movie;
+    //         $movie->addActor($this);
+    //     }
+    //     return $this;
+    // }
+
+    // public function removeMovie(Movies $movie): self
+    // {
+    //     if(!$this->movies->removeElement($movie)){
+    //         $movie->removeActor($this);
+    //     }
+    //     return $this;
+    // }
+}
